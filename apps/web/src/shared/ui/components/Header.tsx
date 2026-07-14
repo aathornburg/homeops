56d95d3d434f9829669365b729966b8e0166245f
Alan Thornburg <alan.thornb@gmail.com>
2026-07-14 19:56:25 -0400

    Reorganized app and added fonts

---
+++ apps/web/src/shared/ui/components/Header.tsx
@@ -0,0 +1,9 @@
+type HeaderProps = {};
+
+export function Header ({}: HeaderProps) {
+    return (
+        <header className="bg-gray-800 text-white p-4">
+            <h1 className="text-xl font-bold">My App</h1>
+        </header>
+    );
+};
---
+++ docs/adr/000X-react-routing-tool.md
@@ -0,0 +1,2 @@
+React Router v7 or Tanstack Router
+https://www.reddit.com/r/reactjs/comments/1klp8cr/react_router_v7_or_tanstack_router/
