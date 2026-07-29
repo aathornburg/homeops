c926cb28f8f993a119e9ad65c5aa790d0f9ade0d
Alan Thornburg <alan.thornb@gmail.com>
2026-07-29 16:13:45 -0400

    More landing page progress

--- apps/web/src/routes/(pre-auth)/landing.tsx
+++ apps/web/src/routes/(pre-auth)/landing.tsx
@@ -1,12 +1,27 @@
-import { Link } from "@tanstack/react-router";
 import { PreAuthHeader } from "../../shared/layout/preAuthHeader";
+import { LinkButton } from "../../shared/ui/linkButton";
 
 export function LandingComponent() {
   return (
     <>
       <PreAuthHeader />
-      <main className="ml-10 max-w-7xl px-4 sm:px-6 lg:px-8">
-        <h1 className="text-5xl w-2/4">Keep the house running, together</h1>
+      <main className="">
+        <div className="flex items-center justify-between bg-[url('/hero-light.png')] dark:bg-[url('/hero-dark.png')] bg-cover bg-center h-fit">
+          <div className="w-3/5 p-20 text-left bg-gradient-to-r from-(color:--color-background-canvas) to-transparent">
+            <h1 className="text-7xl mb-5">Keep the house running, together.</h1>
+            <p className="text-2xl">Track chores, maintenance, and shared household work in one calm place.</p>
+            <div className="mt-5">
+              <LinkButton 
+                to="/"
+                className="rounded font-bold bg-(color:--color-action-primary) px-4 py-2.5 text-(color:--color-background-panel) hover:bg-(color:--color-action-primary-hover)"
+              >Start free</LinkButton>
+              <LinkButton 
+                to="/"
+                className="ml-5 rounded font-bold bg-(color:--color-background-canvas) color px-4 py-2.5 text-(color:--color-text-primary) hover:bg-(color:--color-background-panel) border-2 border-(color:--color-border-default)"
+              >Log in</LinkButton>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   )
